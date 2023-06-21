e6719c62548410a392718ee9ee068f5f82ad5cc5
Guirdo <produccionesaldo@gmail.com>
2023-06-20 21:30:23 -0600

    herencia poo

--- kata-js-avanzado/repaso/poo/index.js
+++ kata-js-avanzado/repaso/poo/index.js
@@ -43,3 +43,85 @@ salvador.saludar()
 console.groupEnd()
 
 // Herencia
+
+// Clase padre
+class Figura {
+  constructor(ancho, largo){
+    this.ancho = ancho
+    this.largo = largo
+  }
+
+  get area() {
+    return this.ancho * this.largo
+  }
+
+  set setAncho(nuevoAncho) {
+    this.ancho = nuevoAncho
+  }
+}
+
+// Clase hijo
+class Cuadrado extends Figura {
+  constructor(lado) {
+    super(lado, lado)
+  }
+}
+
+class Rectangulo extends Figura {
+  constructor(ancho, largo){
+    super(ancho, largo)
+  }
+}
+
+const cuadrado = new Cuadrado(20)
+const rectangulo = new Rectangulo(10,5)
+
+console.log(cuadrado.area)
+console.log(rectangulo.area)
+
+class Circulo extends Figura {
+  constructor(radio) {
+    super(0,0)
+    this.radio = radio
+  }
+
+  get area() {
+    return this.radio * Math.pow(Math.PI, 2) // PI^²
+  }
+
+  get diametro() {
+    return this.radio * 2
+  }
+}
+
+const circulo = new Circulo(4)
+
+console.log(circulo.area)
+console.log(circulo.diametro)
+
+class Triangulo extends Figura{
+  constructor(ancho, largo) {
+    super(ancho,largo)
+  }
+
+  get area() {
+    return (this.ancho * this.largo) / 2
+  }
+}
+
+const triangulo = new Triangulo(3,4)
+console.log(triangulo.area)
+
+/* function Vehiculo(numeroLlantas) {
+  this.numeroLlantas = numeroLlantas
+
+  function getNumeroLlantas(){
+    return this.numeroLlantas
+  }
+
+  return [getNumeroLlantas]
+}
+
+const [getNumeroLlantas] = Vehiculo(4) 
+
+console.log(getNumeroLlantas()) */
